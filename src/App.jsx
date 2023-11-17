4339f367033c930049edef53de6ac4b708a61afd
Constance Essuman <caessuman@st.ug.edu.gh>
2023-11-17 11:01:04 +0000

    Add new feature

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from './components/header/Navbar';
+import Container from './components/container/container.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Container/>
+      {/* Other content */}
+    </div>
+  );
+};
+
+export default App;
